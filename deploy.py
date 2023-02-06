25f17bbcaabffb3cc7aea4af0b4b9154aa353600
kazuhiro MIYASHITA <miyakz1192@gmail.com>
2023-02-06 16:16:36 +0000

    refactoring and extracting data_set.tar.gz supported

--- deploy.py
+++ deploy.py
@@ -35,51 +35,91 @@ class GAALearningOutputDeployManager():
             print(err_msg)
             raise ValueError(err_msg)
 
-    def __deploy_aux(self, dl_type):
-        if not(dl_type in gaa_dl_services):
-            raise ValueError("invalid dl_type")
-        print("INFO: %s" % (dl_type))
-
-    def __print_pwd(self):
-        command = ["pwd"]
-        res = subprocess.check_output(command)
-        print(res)
 
     #targz is TarFile object
-    def __extract_best_weight_file(self, targz):
-        r = re.compile(".*/%s" % (gaa_constants.gaa_best_weight_file_name))
-        weights = []
+    def __extract_file_from_targz(self, targz, target_file_name):
+        r = re.compile(".*/%s" % (target_file_name))
+        targets = []
         for i in targz.getmembers():
             if r.match(i.name):
-                weights.append(i)
+                targets.append(i)
 
         #search newest file
-        if len(weights) == 0:
-            print("ERROR: not best weight file not found")
+        if len(targets) == 0:
+            print("ERROR: %s not found" % (target_file_name))
+            raise ValueError("")
+        elif len(targets) >= 2:
+            print("WARNING: multiple %s found select newest" % (target_file_name))
         
-        best_weight_file = max(weights, key = lambda x: x.mtime)
-        temp_file_name = "./temp/%s" % (gaa_constants.gaa_best_weight_file_name)
+        target_file = max(targets, key = lambda x: x.mtime)
+        temp_file_name = "./temp/%s" % (target_file_name)
         outfile = open(temp_file_name,'wb')
-        outfile.write(targz.extractfile(best_weight_file).read())
+        outfile.write(targz.extractfile(target_file).read())
         outfile.close()
         return temp_file_name
 
-    def deploy(self):
+    #targz is TarFile object
+    def __extract_best_weight_file(self, targz):
+        return self.__extract_file_from_targz(targz,\
+                  gaa_constants.gaa_best_weight_file_name)
+
+    #targz is TarFile object
+    def __extract_data_set_file(self, targz):
+        return self.__extract_file_from_targz(targz,\
+                 gaa_constants.gaa_data_set_file_name)
+
+    def __send_data_set_to_remote(self, dl_type):
         ssh = EasySSHSCP()
+        file_name = self.LOCAL_LEARNING_TASK_OUTPUT_DIR + \
+                    self.deploy_target_task_name + "/" + \
+                    gaa_learning_task_common.dl_image_manager_file_name(dl_type)
+        print("INFO: extracting data set file from %s" % (file_name))
+        targz =  tarfile.open(file_name)
+        data_set_file_name = self.__extract_data_set_file(targz)
+
+        print("INFO: file found, and send it to service")
+        remote_host = dl_type
+        remote_temp_path = gaa_constants.gaa_service_remote_data_set_temp_path[dl_type]
+        ssh.upload(data_set_file_name, remote_host, remote_temp_path)
+
+        remote_data_set_path = gaa_constants.gaa_service_remote_data_set_path[dl_type]
+        remote_temp_path = remote_temp_path + os.path.basename(data_set_file_name)
+
+        print("INFO: %s uploaded successfully" % (remote_temp_path))
+        print("INFO: extract data_set.tar.gz on remote host")
+        res = ssh.ssh(remote_host, "rm -rf %s/*" % (remote_data_set_path))
+        print(res[1]) #print stderr only
+        res = ssh.ssh(remote_host, "tar xvfz %s -C %s" % (remote_temp_path, remote_data_set_path)) 
+        print(res[1])
+        res = ssh.ssh(remote_host, "mv %s/%s/* %s" % (remote_data_set_path, gaa_constants.GAA_DATA_SET_FILE_INTERNAL_DIR, remote_data_set_path)) 
+        print(res[1])
+        res = ssh.ssh(remote_host, "rm -rf %s/%s" % (remote_data_set_path, gaa_constants.GAA_DATA_SET_FILE_INTERNAL_DIR)) 
+        print(res[1])
+
+    def __send_best_weight_to_remote(self, dl_type):
+        ssh = EasySSHSCP()
+        file_name = self.LOCAL_LEARNING_TASK_OUTPUT_DIR + \
+                    self.deploy_target_task_name + "/" + \
+                    gaa_learning_task_common.dl_output_file_name(dl_type)
+
+        print("INFO: extracting best weight file from %s" % (file_name))
+        targz =  tarfile.open(file_name)
+        best_weight_file_name = self.__extract_best_weight_file(targz)
+        
+        print("INFO: file found, and send it to service")
+        remote_host = dl_type
+        remote_path = gaa_constants.gaa_service_remote_weights_path[dl_type]
+        ssh.upload(best_weight_file_name, remote_host, remote_path)
+        print("INFO: %s%s uploaded successfully" % (remote_path,os.path.basename(best_weight_file_name)))
+
+    def deploy(self):
         for dl_type in gaa_constants.gaa_dl_services:
-            file_name = self.LOCAL_LEARNING_TASK_OUTPUT_DIR + \
-                        self.deploy_target_task_name + "/" + \
-                        gaa_learning_task_common.dl_output_file_name(dl_type)
-            print("INFO: extracting best weight file from %s" % (file_name))
-            targz =  tarfile.open(file_name)
-            best_weight_file_name = self.__extract_best_weight_file(targz)
+            print("INFO: trying deploying about %s" % (dl_type))
+            self.__send_best_weight_to_remote(dl_type)
+            self.__send_data_set_to_remote(dl_type)
+            print("INFO: done")
 
-            print("INFO: file found, and send it to service")
-            remote_host = dl_type
-            remote_path = gaa_constants.gaa_service_remote_weights_path[dl_type]
-            ssh.upload(best_weight_file_name, remote_host, remote_path)
 
-            print("INFO: %s%s uploaded successfully" % (remote_path,os.path.basename(best_weight_file_name)))
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
--- gaa_learning_task_common.py
+++ gaa_learning_task_common.py
@@ -9,3 +9,7 @@ def dl_output_file_name(dl_type):
     check_dl_type(dl_type)
     short_file_name = dl_type
     return "%s.tar.gz" % (short_file_name)
+
+def dl_image_manager_file_name(dl_type):
+    check_dl_type(dl_type)
+    return "%s_dl_image_manager.tar.gz" % (dl_type)
